937a900308a7f81dd60842aa2a388d3e833241dc
Tony Rodriguez <tcrod@amazon.com>
2021-06-20 14:30:07 -0700

    Reorganizing/refactoring

--- site/script/main.js
+++ site/script/main.js
@@ -1,19 +1,55 @@
-// Constants.
-// Post DOM-construction actions.
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('`DOMContentLoaded` fired.');
-});
-
 // Only show the first N feed items.
 const FEED_LIMIT = 5;
 
-// Async initialization.
+// Post DOM-construction actions.
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('`DOMContentLoaded` fired.');
+  // Placeholder so I don't have to think about it later.
+});
+
+// Haha, this is overkill for 3 static elements, but I just wanted to see what it looked like.
+// Check it out: an inline invocation of a single instance anonymous stateless React component, using
+// the CDN React (no separate compile => JIT Babel). Better than Handlebars?
+ReactDOM.render(
+  new (()=> {
+    const guidelines = [{
+      "title": "Turn on voice chat",
+      "description": "This greatly assists team coordination and generally improves gameplay.  Plus, it's more fun to chat while we slay.",
+    },{
+      "title": "Clan tag prefix: <code>M4*</code>",
+      "description": "Inspire fear in your opponents! Identify with other clan members! Even bots lie down and accept death when they see us approach! Clan tag is optional but encouraged.",
+    },{
+      "title": "Treat each other kindly",
+      "description": "Recognize and respect each other. Celebrate our diversity and unique talents we bring to the game.",
+    }];
+
+    return (
+      <div class="col">
+        <h2>Clan Guidelines</h2>
+        <dl>
+          {guidelines.map(x => 
+            <React.Fragment>
+            <dt>
+              <i class="bi-check2-circle"></i>
+              <span dangerouslySetInnerHTML={{__html: x.title}}/>
+            </dt>
+            <dd>{x.description}</dd>
+            </React.Fragment>
+          )}
+        </dl>
+      </div>
+    );
+  }),
+  document.getElementById('guidelinesAnchor')
+);
+
+ReactDOM.render(
+  <Slideshow/>,
+  document.getElementById('slideshowAnchor')
+);
+
+// No top level async yet.  Any async task needs to go in here.
 (async () => {
-  ReactDOM.render(
-    <Slideshow/>,
-    document.getElementById('slideshowAnchor')
-  );
 
   const feedData = await (await fetch('script/feedData.json')).json();
 
--- site/script/slideshow.js
+++ site/script/slideshow.js
@@ -1,45 +1,46 @@
 'use strict';
 
-const slides = [
-  {
-    "title": "Water Royale",
-    "caption": "KRED51 leading the clan in swimming exercises",
-    "image": "image/shot_swimming.jpg"
-  },
-  {
-    "title": "Winner!",
-    "caption": "Another great clutch by beaverSTYLE",
-    "image": "image/shot_winner.jpg"
-  }
-];
+const Slideshow = () => {
+  const slides = [
+    {
+      "title": "Water Royale",
+      "caption": "KRED51 leading the clan in swimming exercises",
+      "image": "image/shot_swimming.jpg"
+    },
+    {
+      "title": "Winner!",
+      "caption": "Another great clutch by beaverSTYLE",
+      "image": "image/shot_winner.jpg"
+    }
+  ];
 
-const Slideshow = (props) => {
   return (
-  <div id="actionSlides" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      {slides.map((x, idx) => {
-        let classlist = "carousel-item";
-        if (idx == 0) {
-          classlist += " active"
-        }
-        return (
-          <div class={classlist}>
-            <img src={x.image} class="d-block w-100" alt=""/>
-            <div class="carousel-caption d-none d-md-block">
-              <h5>{x.title}</h5>
-              <p>{x.caption}</p>
+    <div id="actionSlides" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        {slides.map((x, idx) => {
+          let classlist = "carousel-item";
+          if (idx == 0) {
+            classlist += " active"
+          }
+          return (
+            <div class={classlist}>
+              <img src={x.image} class="d-block w-100" alt=""/>
+              <div class="carousel-caption d-none d-md-block">
+                <h5>{x.title}</h5>
+                <p>{x.caption}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#actionSlides" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#actionSlides" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#actionSlides" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#actionSlides" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-)};
+  );
+};
